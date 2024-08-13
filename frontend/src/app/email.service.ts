ad1e35a35a9644bfd271add690636888e3d3d153
watsonm19 <watsonm.dev@gmail.com>
2024-08-13 14:31:26 -0700

    Added email service

---
+++ frontend/src/app/email.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+
+  private apiUrl = 'http://127.0.0.1/:5000/api/generate-subject-lines';
+
+  constructor(private http: HttpClient) { }
+
+  generateSubjectLines(emailContent: string): Observable<string[]> {
+    return this.http.post<string[]>(this.apiUrl, { emailContent });
+  }
+}
