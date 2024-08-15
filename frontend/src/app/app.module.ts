755059cb2dca8a0c5a077be4d094df44acda76eb
watsonm19 <watsonm.dev@gmail.com>
2024-08-15 15:21:01 -0700

    Updated buttons and text area to primeng

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+
 import { CasualSubjectGeneratorComponent } from './casual-subject-generator/casual-subject-generator.component';
 import { FormalSubjectGeneratorComponent } from './formal-subject-generator/formal-subject-generator.component';
 
@@ -21,7 +26,11 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    FloatLabelModule,
     FormsModule,
+    InputTextareaModule,
     RouterModule.forRoot(routes)
   ],
   providers: [provideHttpClient()],
